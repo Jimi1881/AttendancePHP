c622af17a735e9052769e1e9c2fa2d4353fa2540
Jimi1881 <77850210+Jimi1881@users.noreply.github.com>
2021-02-18 20:16:42 +0000

    Update conn.php

--- includes/db/conn.php
+++ includes/db/conn.php
@@ -1,9 +1,17 @@
 <?php
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
-    $charset = 'utf8mb4';
+ //  Development connectio
+  //  $host = '127.0.0.1';
+  //  $db = 'attendance_db';
+  //  $user = 'root';
+  //  $pass = '';
+  //  $charset = 'utf8mb4';
+
+  // Remote connection
+     $host = 'remotemysql.com';
+     $db = 'qdqCoZft4D';
+     $user = 'qdqCoZft4D';
+     $pass = 'GSHa5MEqSR';
+     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host; dbname=$db; charset=$charset";
 
